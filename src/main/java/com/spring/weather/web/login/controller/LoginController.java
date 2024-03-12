bf0250a8be4e75e9be93892755b7e13a3abe5a4e
OJS <onionpeeel@gmail.com>
2024-03-12 11:07:02 +0900

    로그인 및 필터 적용

--- src/main/java/com/spring/weather/web/login/controller/LoginController.java
+++ src/main/java/com/spring/weather/web/login/controller/LoginController.java
@@ -1,13 +1,43 @@
 package com.spring.weather.web.login.controller;
 
+import com.spring.weather.web.login.model.LoginVO;
+import com.spring.weather.web.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
 @RestController
 @RequestMapping(path = "/api/login")
 public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<Map<String, Object>> getUserInfo(@ModelAttribute LoginVO loginVO) throws Exception {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("success", true);
+
+        // 회원정보 검증 및 토큰 생성
+        LoginVO userInfo = loginService.findLoginUserInfo(loginVO.getUserId(), loginVO.getUserPwd());
+
+        if (ObjectUtils.isEmpty(userInfo)) {
+            resultMap.put("success", false);
+            return ResponseEntity.ok(resultMap);
+        }
+
+        resultMap.put("userInfo", userInfo);
+
+        return ResponseEntity.ok(resultMap);
+    }
 }
--- src/main/java/com/spring/weather/web/login/service/LoginService.java
+++ src/main/java/com/spring/weather/web/login/service/LoginService.java
@@ -1,10 +1,15 @@
 package com.spring.weather.web.login.service;
 
+import com.spring.weather.common.security.jwt.JwtTokenProvider;
 import com.spring.weather.web.login.mapper.LoginMapper;
+import com.spring.weather.web.login.model.LoginVO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Transactional
@@ -18,5 +23,35 @@ public class LoginService {
     private final LoginMapper loginMapper;
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public LoginVO findLoginUserInfo(String userId, String userPwd) {
+        LoginVO loginVO = loginMapper.selectLoginInfo(userId);
+
+        if (loginVO == null) throw new RuntimeException("fail");
+
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, userPwd);
+
+            // 검증
+            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+            // JWT 토큰 생성
+            String token = jwtTokenProvider.generateToken(authentication);
+
+            String refreshToken = jwtTokenProvider.refreshToken(authentication);
+
+            loginVO.setToken(token);
+            loginVO.setRefreshToken(refreshToken);
+
+            return loginVO;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("fail");
+        }
+    }
 
 }
